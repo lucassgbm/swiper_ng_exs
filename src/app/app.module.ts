cdf5ce9a756834c47cf945df0ac66d811b22c252
Lucas <lucassgbm@hotmail.com>
2021-12-07 16:12:01 -0300

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { SwiperModule } from "swiper/angular";
 
 import { AppComponent } from './app.component';
+import { SwiperComponent } from './swiper/swiper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, FormsModule, SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
